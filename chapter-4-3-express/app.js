20dba550fca1998fb389a4ca378a18e99c17dbb0
Ari Susanto <risusanto@outlook.com>
2022-08-01 21:46:33 +0700

    add get input (headers, path parameters, query parameters)

--- chapter-4-3-express/app.js
+++ chapter-4-3-express/app.js
@@ -5,16 +5,10 @@ const app = express()
 const host = 'localhost';
 const port = 8080;
 
-let handler1 = function (req, res) {
+
+app.get('/', function (req, res) {
     res.send('Hello World')
-}
-
-let handler2 = function (req, res) {
-    res.send('Ini hello')
-}
-
-app.get('/', handler1)
-app.get('/hello', handler2)
+})
 
 // ini cara pakai router yg didefinisikan di route.js
 app.use('/info',info_router)
--- chapter-4-3-express/route.js
+++ chapter-4-3-express/route.js
@@ -2,11 +2,42 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res) {
-    res.send("My Server localhost")
+    let username = req.headers['username']
+    if (username === undefined) {
+        res.status(401)
+        return res.send("unauthorized")
+    }
+    res.send(`Hello ${username}, welcome back!`)
 })
 
 router.get('/time', function (req, res) {
     res.send(new Date().toISOString())
+})
+
+// ini bakalan diaksesnya seperti ini->  http://localhost:8080/info/check-id?name=Ari&age=17&query=dasdasdas
+router.get('/check-id', function (req, res) {
+    let name = req.query["name"]
+    let age = parseInt(req.query["age"])
+    let query = req.query["query"]
+
+    res.send(`Name: ${name} | Age: ${age} | Query: ${query}`)
+})
+
+// contoh cara akses->  http://localhost:8080/info/hitung-luas?p=5&l=2
+router.get('/hitung-luas', function (req, res) {
+    let panjang = parseInt(req.query["p"])
+    let lebar  = parseInt(req.query["l"])
+
+    let luas = panjang * lebar
+
+    res.send(`luas persegi panjang: ${luas}`)
+})
+
+
+// path parameter, cara akses contoh  http://localhost:8080/info/get-lastname/susanto atau  http://localhost:8080/info/get-lastname/smith
+router.get('/get-lastname/:lastname', function (req, res) {
+    let lastname = req.params["lastname"]
+    res.send(`Lastname: ${lastname}`)
 })
 
 module.exports = router
