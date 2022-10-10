c7c8f60c91fb2bd5240cc52f3925f0abd9917d37
Ari Susanto <risusanto@outlook.com>
2022-10-10 20:54:18 +0700

    websocket: using constant instead of hardcoded event listener and emitter

--- chapter-9-1-ws/app.js
+++ chapter-9-1-ws/app.js
@@ -6,6 +6,16 @@ let server = app.listen(3000, () => {
     console.log('server running at http://localhost:3000')
 })
 
+const LISTENER = {
+    ChatMessage: 'chatMessage',
+    Connect: 'connection',
+    Disconnect: 'disconnect'
+}
+
+const EMITTER = {
+    OnNewMessage: 'onNewMessage'
+}
+
 // init socket io
 const io = socketIO(server)
 
@@ -18,22 +28,22 @@ io.use((socket, next) => {
     next()
 })
 
-io.on('connection', (socket) => {
+io.on(LISTENER.Connect, (socket) => {
     let username = socket.handshake.auth.username
     console.log(`${username} connected`)
 
     socket.join(username)
 
     // event listener chatMessage
-    socket.on('chatMessage', (msg) => {
+    socket.on(LISTENER.ChatMessage, (msg) => {
         let message = {
             from: username,
             ...msg
         }
-        socket.to(message.to).emit('onNewMessage', message)
+        socket.to(message.to).emit(EMITTER.OnNewMessage, message)
     })
 
-    socket.on('disconnect', () => {
+    socket.on(LISTENER.Disconnect, () => {
         console.log(`${username} disconnected`)
     })
 })
